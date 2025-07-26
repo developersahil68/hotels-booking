e5ae81aa74729096f83cc807c520ad20dcbff3be
developersahil68 <sahil852700@gmail.com>
2025-07-27 01:01:17 +0530

    learned how server and client components interact with each other , how to make a fetch req in the server component

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,8 +1,25 @@
+// eslint@typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import React from "react";
 import { NextPage } from "next";
+import Counter from "../components/Counter";
 
-const Page: NextPage = () => {
-  return <h1>Cabins page</h1>;
+const Page: NextPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <div>
+      <h1>Cabins page</h1>;
+      <ul>
+        {data.map((user: any) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
+    </div>
+  );
 };
 
 export default Page;
